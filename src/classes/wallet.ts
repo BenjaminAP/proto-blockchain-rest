c38c9411fa7ccacec3775d40a99a01e4a6849c27
Benjamin <patonacevedo@gmail.com>
2021-04-04 16:00:52 -0400

    started with own wallet need to move to front end

--- src/classes/wallet.ts
+++ src/classes/wallet.ts
@@ -1,4 +1,14 @@
 
+const EdDSA = require('elliptic').eddsa
+
+const ec = new EdDSA('ed25519');
+
+let mnemonic = 'something strange in my room'/// generate with bip39;
+
+const key = ec.keyFromSecret(Buffer.from(JSON.stringify(mnemonic), 'hex'));
+
+console.log(key);
+
 const  bitcointLib = require('bitcoinjs-lib');
 
 export class Wallet {
@@ -7,3 +17,8 @@ export class Wallet {
         
         }
 }
+
+
+/// TODO: yarn install elliptic
+/// llok for mnemonic generator = bip39
+
